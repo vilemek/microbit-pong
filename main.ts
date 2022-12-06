08f3ade5293c52cfc82216f3996fa35f3e2835d8
vili116 <36635527+vilemek@users.noreply.github.com>
2022-12-06 11:19:09 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,6 +5,8 @@ let bx = 0
 let by = 0
 let dirx = 0
 let diry = 0
+let bx1 = 0
+let by1 = 0
 input.onButtonPressed(Button.A, function () {
     if (player_pos_1 < 1) {
         player_pos_1 += 1
@@ -35,58 +37,58 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
 })
 loops.everyInterval(2500, function () {
     if (gamp == 1) {
+        bx1 = bx
+        by1 = by
         led.unplot(bx, by)
         if (dirx == 1) {
             bx += 1
-            if (bx == 3) {
-                if (led.point(bx + dirx, by + diry)) {
-                    dirx = -1
-                    if (diry == 1) {
-                        diry = -1
-                    } else if (diry == -1) {
-                        diry = 1
-                    } else {
-                    	
-                    }
-                } else {
-                    gamp = 0
-                }
-            }
-        } else if (dirx == 1) {
+        } else if (dirx == -1) {
             bx += -1
-            if (bx == 1) {
-                if (led.point(bx + dirx, by + diry)) {
-                    dirx = 1
-                    if (diry == 1) {
-                        diry = -1
-                    } else if (diry == -1) {
-                        diry = 1
-                    } else {
-                    	
-                    }
-                } else {
-                    gamp = 0
-                }
-            }
         } else {
         	
+        }
+        if (bx == 3 || bx == 1) {
+            if (led.point(bx1 + dirx, by1 + diry)) {
+                if (diry == 1) {
+                    diry = -1
+                } else if (diry == -1) {
+                    diry = 1
+                }
+                if (dirx == 1) {
+                    dirx = -1
+                } else if (dirx == -1) {
+                    dirx = 1
+                }
+            } else {
+            	
+            }
         }
         if (diry == 1) {
-            by += 1
-            if (by == 5) {
+            if (by + diry >= 5) {
                 diry = -1
-                by = 4
             }
+            by += 1
         } else if (diry == -1) {
-            by += -1
-            if (by == -1) {
-                diry = 1
-                by = 0
+            if (by + diry <= -1) {
+                diry = -1
             }
-            led.plot(bx, by)
+            by += -1
         } else {
         	
         }
+        if (bx >= 4) {
+            dirx = -1
+        } else if (bx <= 0) {
+            dirx = 1
+        }
+        if (by >= 5) {
+            by = 4
+            diry = -1
+        } else if (by <= -1) {
+            by = 0
+            diry = 1
+        }
+        led.plot(bx, by)
     } else if (gamp == 5) {
         led.plot(2, 2)
         basic.pause(1000)
